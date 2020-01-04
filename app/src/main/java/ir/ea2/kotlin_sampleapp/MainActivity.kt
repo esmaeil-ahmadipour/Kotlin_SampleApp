a326a767f5e2ed0098ebc22ae3cd68aa57a85cc5
software8686 <software8686@gmail.com>
2020-01-04 07:16:04 +0330

    commit> initialize views  mood :  Bind View Object

--- app/src/main/java/ir/ea2/kotlin_sampleapp/MainActivity.kt
+++ app/src/main/java/ir/ea2/kotlin_sampleapp/MainActivity.kt
@@ -1,21 +1,20 @@
 package ir.ea2.kotlin_sampleapp
 
-import android.app.ProgressDialog.show
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import android.widget.Toast
 import android.widget.Toast.LENGTH_LONG
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-private val btn:Button by lazy{
-    findViewById<Button>(R.id.ac_main_btn)
-}
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        btn.setOnClickListener{Toast.makeText(this,resources.getString(R.string.toast_test_message),LENGTH_LONG).show() }
+        // Goodbye findViewById ! We Now Bind View Object On RunTime! (by Using plugin: 'kotlin-android-extensions' in header of file build.gradle)
+        ac_main_btn.setOnClickListener{Toast.makeText(this,resources.getString(R.string.toast_test_message),LENGTH_LONG).show()
+        ac_main_txt.text="Hello Beautiful World :)"
+        }
     }
 
 
