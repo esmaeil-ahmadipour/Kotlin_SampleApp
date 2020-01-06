18e0eeb630138731d1c0ae932c69e70d59b3ec49
software8686 <software8686@gmail.com>
2020-01-06 05:21:04 +0330

    commit> Handler & Runnable> Timer App with Start & Reset Button

--- app/src/main/java/ir/ea2/kotlin_sampleapp/MainActivity.kt
+++ app/src/main/java/ir/ea2/kotlin_sampleapp/MainActivity.kt
@@ -1,110 +1,43 @@
 package ir.ea2.kotlin_sampleapp
 
-import android.Manifest
-import android.content.Context
-import android.content.Intent
-import android.content.pm.PackageManager
-import android.net.Uri
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
+import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import ir.ea2.kotlin_sampleapp.util.toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    var context: Context = this
-    private val phoneNumber: String = "+989163902136"
-
-    companion object {
-        // Final Variable Used As Key For Intent.putExtra.
-        const val INPUT_VALUE = "INPUT"
-        //Permission  requestCode callPhone   REQUEST_CODE_CALL_PHONE
-        const val REQUEST_CODE_CALL_PHONE: Int = 100
-    }
+    var number: Int = 0
+    var handler: Handler = Handler()
+    lateinit var runnable: Runnable
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        ac_main_toolbar.title = resources.getString(R.string.app_name)
-        setSupportActionBar(ac_main_toolbar)
         listeners()
     }
 
+    private fun start() {
+        number = 0
+        ac_main_txt.text = "Number is $number"
+        //object is one SingletonObject / implement from Runnable
+        runnable = object : Runnable {
+            override fun run() {
+                number++
+                ac_main_txt.text = "Number is $number"
+                handler.postDelayed(runnable, 1000)
+            }
+        }
+        handler.postDelayed(runnable, 1000)
+    }
+
+    private fun reset() {
+        handler.removeCallbacks(runnable)
+        number = 0
+        ac_main_txt.text = "Number is $number"
+    }
+
     private fun listeners() {
-        // Goodbye findViewById ! We Now Bind View Object On RunTime! (by Using plugin: 'kotlin-android-extensions' in header of file build.gradle)
-        ac_main_btn.setOnClickListener {
-            //Using ContextExtensions From Package util .
-            this.toast(resources.getString(R.string.toast_test_message))
-            ac_main_txt.text = "Hello Beautiful World :)"
-            startSecondActivity()
-        }
-
-        //Call & PermissionCheck
-        ac_main_btn_call.setOnClickListener {
-            if (checkCallPhonePermission()) {
-                intentCallPhone()
-            } else {
-                callPhonePermission()
-            }
-        }
-    }
-    //After Send Any requestPermissions Run This Method For Check Results.
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray) {
-        when (requestCode) {
-            REQUEST_CODE_CALL_PHONE -> {
-                if (grantResults.isNotEmpty()) {
-                    if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                        // CallPhone is Allow
-                        intentCallPhone()
-                    }
-                }
-            }
-        }
-    }
-
-    private fun intentCallPhone() {
-        val intentCall = Intent(Intent.ACTION_CALL)
-        intentCall.data = Uri.parse("tel:$phoneNumber")
-        startActivity(intentCall)
-    }
-
-    //Inflate Layer PopupMenu :
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        super.onCreateOptionsMenu(menu)
-        menuInflater.inflate(R.menu.popup_menu, menu)
-        return true
-    }
-
-    //When Click On One Item :
-    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
-        super.onOptionsItemSelected(item)
-        when (item?.itemId) {
-            R.id.menu_item_1 -> this.toast(item.title)
-            R.id.menu_item_2 -> this.toast(item.title)
-            R.id.menu_item_3 -> this.toast(item.title)
-            R.id.menu_item_4 -> this.toast(item.title)
-            R.id.menu_icon_search -> this.toast(item.title)
-        }
-        return true
-    }
-
-    //Get Permission From User'sOfApplication:
-    private fun callPhonePermission() =
-        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CALL_PHONE), REQUEST_CODE_CALL_PHONE)
-
-    //Check Permission Accepted by User'sOfApplication Or Rejected:
-    private fun checkCallPhonePermission(): Boolean =
-        ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED
-
-    private fun startSecondActivity() {
-        val intent = Intent(this, SecondActivity::class.java)
-        intent.putExtra(INPUT_VALUE, ac_main_edt.text.toString())
-        startActivity(intent)
+        ac_main_btn_reset.setOnClickListener() { reset() }
+        ac_main_btn_start.setOnClickListener() { start() }
     }
 }
