909da80deec73d8e99e216e89ec6a24226c203de
software8686 <software8686@gmail.com>
2020-01-06 04:04:20 +0330

    commit> AlertDialog>

--- app/src/main/java/ir/ea2/kotlin_sampleapp/SecondActivity.kt
+++ app/src/main/java/ir/ea2/kotlin_sampleapp/SecondActivity.kt
@@ -1,18 +1,41 @@
 package ir.ea2.kotlin_sampleapp
 
+import android.content.DialogInterface
 import android.os.Bundle
-import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.toColor
+import ir.ea2.kotlin_sampleapp.util.toast
+import kotlinx.android.synthetic.main.activity_second.*
 
 class SecondActivity : AppCompatActivity() {
-    private var txtView: TextView? = null
+    //onCreate
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
-        txtView = findViewById(R.id.ac_second_txt)
+        initialize()
+        listeners()
+    }
+
+    private fun listeners() {
+        ac_second_btn?.setOnClickListener { showAlertDialog() }
+    }
+
+    private fun showAlertDialog() {
+        val alert = AlertDialog.Builder(this)
+        alert.setTitle(resources.getString(R.string.alert_second_title))
+        alert.setMessage(resources.getString(R.string.alert_second_message))
+        alert.setNegativeButton(resources.getString(R.string.alert_second_negative)) { dialogInterface: DialogInterface?, i: Int -> this.toast(resources.getString(R.string.alert_second_negative)) }
+        //Q: What is mean "_" in this code ?   A: We Say To Kotlin This Parameter is never Used.
+        alert.setPositiveButton(resources.getString(R.string.alert_second_positive)) { _: DialogInterface?, _: Int -> this.toast(resources.getString(R.string.alert_second_positive)) }
+        alert.show()
+    }
+
+
+    private fun initialize() {
         // this "!!" for Confirm (isn't NullObject)
-        txtView!!.text = (this.title)
+        ac_second_txt?.text = (this.title)
         // If This Object Get Null Value , Then You Must change "!!" to "?"
-        txtView?.text = intent.getStringExtra(MainActivity.INPUT_VALUE)
+        ac_second_txt?.text = intent.getStringExtra(MainActivity.INPUT_VALUE)
     }
 }
