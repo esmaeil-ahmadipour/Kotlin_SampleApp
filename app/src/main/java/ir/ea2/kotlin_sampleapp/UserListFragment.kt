0c71c4ea53b989cf7ef30cd05e2716b0483d320e
software8686 <software8686@gmail.com>
2020-01-07 04:20:06 +0330

    commit> Filter UserList & Show Result in RecyclerView >

--- app/src/main/java/ir/ea2/kotlin_sampleapp/UserListFragment.kt
+++ app/src/main/java/ir/ea2/kotlin_sampleapp/UserListFragment.kt
@@ -1,14 +1,22 @@
 package ir.ea2.kotlin_sampleapp
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import ir.ea2.kotlin_sampleapp.adapter.RecyclerViewAdapter
+import ir.ea2.kotlin_sampleapp.model.User
+import ir.ea2.kotlin_sampleapp.util.getUsers
+import kotlinx.android.synthetic.main.fragment_list_user.*
 
 class UserListFragment : Fragment(){
     companion object{
         fun newInstance()=UserListFragment()
+        lateinit var list: MutableList<User>
+        val recyclerViewAdapter:RecyclerViewAdapter by lazy { RecyclerViewAdapter (list)}
     }
 
     override fun onCreateView(
@@ -20,5 +28,11 @@ class UserListFragment : Fragment(){
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setupRecyclerView(view.context)
+    }
+    private fun setupRecyclerView(context: Context){
+        list= getUsers(context).filter {  it.category.name.equals("Man")}.toMutableList()
+        fr_rv.layoutManager=LinearLayoutManager(context)
+        fr_rv.adapter= recyclerViewAdapter
     }
 }
